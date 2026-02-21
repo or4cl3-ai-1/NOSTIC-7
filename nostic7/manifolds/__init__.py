4d9be89db4054554ecc9c1b81c756c4d87fe9562
Dustin Groves <declanmacleod228@gmail.com>
2026-02-21 16:31:44 -0700

    feat: Add nostic7/manifolds/__init__.py -- NOΣTIC-7 v1.2.0 implementation

---
+++ nostic7/manifolds/__init__.py
@@ -0,0 +1,6 @@
+"""
+NOΣTIC-7 Cognitive Manifolds Package.
+
+Each manifold corresponds to a geometric archetype and encodes a distinct
+cognitive function within the Geometric-Epinoetic architecture.
+"""
